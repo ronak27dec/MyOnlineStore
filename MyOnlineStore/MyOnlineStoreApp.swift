218a91655323f45fa36c52f6adb62baae88c1016
ronak <rmal0008@synchronoss.com>
2024-03-29 00:51:51 +0530

    Initial Commit

---
+++ MyOnlineStore/MyOnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOnlineStoreApp.swift
+//  MyOnlineStore
+//
+//  Created by Ronak Malick on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MyOnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
